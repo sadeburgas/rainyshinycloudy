554f8a27e28bdf63edf265edd00de4522d011e5a
Dilyan Ergeledzhiev <dergeledji@gmail.com>
2017-07-23 02:25:22 +0200

    Creating UI with IBOutlet complete

---
+++ rainyshinycloudy/WeatherVC.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  rainyshinycloudy
+//
+//  Created by sade on 22/7/17.
+//  Copyright © 2017 sade. All rights reserved.
+//
+
+import UIKit
+
+class WeatherVC: UIViewController {
+    
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var currentTempLabel: UILabel!
+    @IBOutlet weak var locationLabel: UILabel!
+    @IBOutlet weak var currentWeatherImage: UIImageView!
+    @IBOutlet weak var currentWeatherTypeLabel: UILabel!
+    @IBOutlet weak var tableView:UITableView!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       
+    }
+
+   
+
+
+}
+
